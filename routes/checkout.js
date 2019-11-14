2af719910547fe173cbe5f76cf049e628537f3e2
Gilad Waldman <gw929@nyu.edu>
2019-11-13 21:41:38 -0500

    Working Braintree sandbox payment.

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'h8bcz46ctqcysn42',
+    publicKey: '2v8xgm2gtqnnjj7y',
+    privateKey: '8e290893b465ed3f4a6d7c10dba696db'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  var newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
